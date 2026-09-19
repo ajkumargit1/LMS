b3ea64e3de3e94bdd95b931216c9319d43573477
Ajay Kumar <ajay@example.com>
2026-09-19 23:32:30 +0530

    user model added

---
+++ server/models/User.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import mongoose from 'mongoose'
+import Course from './Course.js';
+
+
+const userSchema=new mongoose.Schema(
+    {
+        _id: {type: String, required: true},
+        name:{type: String, required:true},
+        email:{type: String, required:true},
+        imageUrl:{type:String, required:true},
+        enrolledCourses:[
+            {
+                type:mongoose.Schema.Types.ObjectId,
+                ref:'Course'
+            }
+        ],
+    },{timestamps:true}
+);
+
+const User=mongoose.model('User',userSchema);
+
+export default User
